7ff879962ecbf2a3999169d646461823f0ec86fa
Cendy <cendyemail@gmail.com>
2024-12-02 15:17:16 +0700

    misc: init gui

---
+++ oregon-backend/oregon-backend/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace oregon_backend;
+
+class Program
+{
+    private static Sdl2Window _window;
+    private static GraphicsDevice _gd;
+    private static ImGuiController _controller;
+    private static CommandList _cl;
+    
+    private static Vector3 _clearColor = new(0.45f, 0.55f, 0.6f);
+    
+    private static void Main(string[] args)
+    {
+        VeldridStartup.CreateWindowAndGraphicsDevice(
+            new WindowCreateInfo(50, 50, 800, 400, WindowState.Normal, "Oregon - Backend"),
+            new GraphicsDeviceOptions(true, null, true, ResourceBindingModel.Improved, true, true),
+            out _window,
+            out _gd);
+        _window.Resized += () =>
+        {
+            _gd.MainSwapchain.Resize((uint)_window.Width, (uint)_window.Height);
+            _controller.WindowResized(_window.Width, _window.Height);
+        };
+        _cl = _gd.ResourceFactory.CreateCommandList();
+        _controller = new ImGuiController(_gd, _gd.MainSwapchain.Framebuffer.OutputDescription, _window.Width, _window.Height);
+        
+        var stopwatch = Stopwatch.StartNew();
+        var deltaTime = 0f;
+        
+        while (_window.Exists)
+        {
+            deltaTime = stopwatch.ElapsedTicks / (float)Stopwatch.Frequency;
+            stopwatch.Restart();
+            var snapshot = _window.PumpEvents();
+            if (!_window.Exists) { break; }
+            _controller.Update(deltaTime, snapshot);
+
+            // SubmitUI();
+
+            _cl.Begin();
+            _cl.SetFramebuffer(_gd.MainSwapchain.Framebuffer);
+            _cl.ClearColorTarget(0, new RgbaFloat(_clearColor.X, _clearColor.Y, _clearColor.Z, 1f));
+            _controller.Render(_gd, _cl);
+            _cl.End();
+            _gd.SubmitCommands(_cl);
+            _gd.SwapBuffers(_gd.MainSwapchain);
+        }
+        
+        _gd.WaitForIdle();
+        _controller.Dispose();
+        _cl.Dispose();
+        _gd.Dispose();
+    }
+}
