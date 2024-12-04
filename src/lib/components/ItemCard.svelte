1417637f98a8a763928d8ee56106d5d6661f089f
Ben Wu <ben@jxcb.xyz>
2024-12-04 10:53:37 +0700

    chrome compability

--- src/lib/components/ItemCard.svelte
+++ src/lib/components/ItemCard.svelte
@@ -1,14 +1,16 @@
-<div
-	class="grid h-72 w-40 flex-shrink-0 flex-grow-0 grid-cols-1 grid-rows-7 rounded border border-black"
->
-	<div class="row-span-4 bg-slate-500"></div>
-	<div class="row-span-3 flex flex-col justify-between px-3 py-3 shadow-inner">
-		<div>
-			<p class="font-light">product name</p>
-			<p class="font-medium text-green-800">Rp50.000</p>
-		</div>
-		<div>
-			<p class="text-right">4.8/5 <i class="fa-solid fa-star"></i></p>
+<a href="/product">
+	<div
+		class="grid h-72 w-40 flex-shrink-0 flex-grow-0 grid-cols-1 grid-rows-7 rounded border border-black"
+	>
+		<div class="row-span-4 bg-slate-500"></div>
+		<div class="row-span-3 flex flex-col justify-between px-3 py-3 shadow-inner">
+			<div>
+				<p class="font-light">product name</p>
+				<p class="font-medium text-green-800">Rp50.000</p>
+			</div>
+			<div>
+				<p class="text-right">4.8/5 <i class="fa-solid fa-star"></i></p>
+			</div>
 		</div>
 	</div>
-</div>
+</a>
--- src/routes/(storefront)/+layout.svelte
+++ src/routes/(storefront)/+layout.svelte
@@ -8,11 +8,9 @@
 </svelte:head>
 
 <div class="flex h-auto w-svw flex-col content-center items-center overflow-hidden">
-	<div class="container m-0 flex h-full w-full flex-col border border-black p-0 md:w-[480px]">
+	<div class="container m-0 flex h-full w-full flex-col p-0 md:w-[480px]">
 		<header class="flex h-16 w-full flex-row items-center justify-between px-6 shadow-md">
-			<a href="admin">
-				<h1 class="font-serif text-2xl md:text-3xl">0regon</h1>
-			</a>
+			<h1 class="font-serif text-2xl md:text-3xl">0regon</h1>
 			<nav class="flex justify-between gap-2 md:gap-4">
 				<a href="/">Home</a>
 				<a href="/">Categories</a>
