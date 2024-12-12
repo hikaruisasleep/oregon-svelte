f685253e12a1081fe6a25e58afe86efbd0ef753e
Ben Wu <ben@jxcb.xyz>
2024-12-12 18:03:00 +0700

    disable csrf so tokens stay

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,8 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			out: 'build'
-		})
+		}),
+		csrf: { checkOrigin: false }
 	},
 
 	compilerOptions: {
